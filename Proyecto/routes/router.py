bd7da40a9d38c1c7260cddbaeb743d94ead5fa20
Elias <elias.poma@unl.edu.ec>
2025-01-09 21:43:21 -0500

    Vistas

--- Proyecto/routes/router.py
+++ Proyecto/routes/router.py
@@ -25,45 +25,62 @@ cors = CORS(router, resource={
 def inicio():
     return render_template('inicio.html')
 
-
 #------------ Vista Estudiante-----------------#
-@router.route('/estudiante')
+@router.route('/estudiante', methods=['GET'])
 def estudiante():
     return render_template('/estudiante/estudiante.html')
 
+@router.route('/estudiante/inicioEstudiante', methods=['GET'])
+def inicioEstudiante():
+    return render_template('/estudiante/inicioEstudiante.html')
 
+@router.route('/estudiante/perfil', methods=['GET'])
+def perfil():
+    return render_template('/estudiante/perfil.html')
 
 #------------ Vista Docente-----------------#
-@router.route('/docente')
-def docente():
-    return render_template('/docente/docente.html')
 
 
-@router.route('/administrador')
+@router.route('/docente/docenteInicio', methods=['GET'])
+def docenteInicio():
+    return render_template('/docente/docenteInicio.html')
+
+@router.route('/docente/crearTarea', methods=['GET'])
+def crearTarea():
+    return render_template('/docente/crearTarea.html')
+
+@router.route('/docente/eliminarAsignados', methods=['GET'])
+def eliminarAsignados():
+    return render_template('/docente/eliminarAsignados.html')
+
+@router.route('/docente/evaluacionEstres', methods=['GET'])
+def evaluacionEstres():
+    return render_template('/docente/evaluacionEstres.html')
+
+@router.route('/docente/gestionGeneral', methods=['GET'])
+def gestionGeneral():
+    return render_template('/docente/gestionGeneral.html')
+
+#---------------------------------------------Administrador-----------------------------------------------------#
+@router.route('/administrador', methods=['GET'])
 def administrador():
-    return render_template('administrador/administrador.html')  # Asegúrate de que esta ruta sea correcta
+    return render_template('administrador/administrador.html')
 
-# Página para gestionar usuarios
-@router.route('/administrador/gestionar_usuarios')
+@router.route('/administrador/gestionar_usuarios', methods=['GET'])
 def gestionar_usuarios():
     return render_template('administrador/gestionar_usuarios.html')
 
-# Página para gestionar técnicas de reducción de estrés
-@router.route('/administrador/gestionar_tecnicas')
+@router.route('/administrador/gestionar_tecnicas', methods=['GET'])
 def gestionar_tecnicas():
     return render_template('administrador/gestionar_tecnicas.html')
 
-# Página para configurar alertas
-@router.route('/administrador/configurar_alertas')
+@router.route('/administrador/configurar_alertas', methods=['GET'])
 def configurar_alertas():
     return render_template('administrador/configurar_alertas.html')
 
-# Página para visualizar datos generales
-@router.route('/administrador/visualizar_datos')
+@router.route('/administrador/visualizar_datos', methods=['GET'])
 def visualizar_datos():
     return render_template('administrador/visualizar_datos.html')
 
 
-@router.route('/estudiante/inicioEstudiante')
-def inicioEstudiante():
-    return render_template('/estudiante/inicioEstudiante.html')
+
