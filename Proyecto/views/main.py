96d191a7f0de860e72e72fda20957120a9a06205
Darwin Andrés Sarango Soto <darwin.a.sarango@unl.edu.ec>
2025-01-14 00:19:59 -0500

    Corrección de vistas estudiante

--- Proyecto/views/main.py
+++ Proyecto/views/main.py
@@ -11,26 +11,26 @@ cc = CuentaControl()
 uc = UsuarioControl()
 
 #PIMERO CREAMOS EL USUARIO
-# uc._usuario._nombre = "Alexander"
-# uc._usuario._apellido = "Sanchez"
-# uc._usuario._ci = "1104753890"
-# uc._usuario._fechaNacimiento = "02/05/2004" #IMPORTANTE EL FORMATO ES DD/MM/YYYY
-# uc._usuario._direccion = "Buenos Aires y 9 de Octubre" 
-# uc._usuario._telefono = "980679607"
+# uc._usuario._nombre = "Darwin"
+# uc._usuario._apellido = "Sarango"
+# uc._usuario._ci = "1105630030"
+# uc._usuario._fechaNacimiento = "20/06/2004" #IMPORTANTE EL FORMATO ES DD/MM/YYYY
+# uc._usuario._direccion = "Obrapia Menfis Central" 
+# uc._usuario._telefono = "0964209135"
 # uc.save
 
 #SEGUNDO CREAMOS LA CUENTA
-# cc._cuenta._correo = "alexander.sanchez@unl.edu.ec"
+# cc._cuenta._correo = "darwin.a.sarango@unl.edu.ec"
 # cc._cuenta._contrasena = "1234"
 # cc._cuenta._estado = 1
-# cc._cuenta._idUsuario = 3
+# cc._cuenta._idUsuario = 1
 # cc.save
 
 #TERCERO CREAMOS EL ROL
-# rc._rol._nombre = "Docente"
-# rc._rol._detalle = "Rol para los docentes"
+# rc._rol._nombre = "Estudiante"
+# rc._rol._detalle = "Rol para los estudiantes"
 # rc._rol._estado = 1
-# rc._rol._idCuenta = 3
+# rc._rol._idCuenta = 1
 # rc.save
 
 
